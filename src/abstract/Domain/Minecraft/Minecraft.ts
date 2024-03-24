a910a201bac5ecf4b2287998cf5573c4925570b4
kage1020 <4620087@ed.tus.ac.jp>
2024-03-25 00:21:06 +0900

    add version validation

--- src/abstract/Domain/Minecraft/Minecraft.ts
+++ src/abstract/Domain/Minecraft/Minecraft.ts
@@ -1,13 +1,18 @@
-import { Version } from '@/abstract/Domain/Version/Version';
+import type { ValidVersion as Version } from '@/abstract/Domain/Version/Version';
 
 export abstract class Minecraft {
   private _version: Version;
 
   public constructor(version: Version) {
+    if (!this.isValidVersion(version)) {
+      throw new Error('Invalid version');
+    }
     this._version = version;
   }
 
   public get version(): Version {
     return this._version;
   }
+
+  protected abstract isValidVersion(version: Version): boolean;
 }
